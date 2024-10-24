81339706218f265a9d660094f308a7696468f9a1
overskilled <ouateedemloic@gmail.com>
2024-10-24 13:41:39 +0100

    Update route

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,6 @@ import NotificationSettings from "./pages/NotificationSettings/Notification"
 import UpdatePersonalInfo from "./pages/Info/Info"
 import ScheduleAppointment from "./pages/Appointement/Appointement"
 import DonationHistory from "./pages/History/History"
-import BloodBankDashboard from "./pages/BloodBankDashboard/Dashboard"
-import BloodStockManagement from "./pages/BloodBankDashboard/BloodStockManagement"
-import TrackingBloodRequests from "./pages/BloodBankDashboard/TrackingBloodRequest"
-import CertifyingDonors from "./pages/BloodBankDashboard/CertifyingDonor"
-import SendingNotificationsToDonors from "./pages/BloodBankDashboard/SendingNotificationsToDonors"
-import CoordinationWithHospitals from "./pages/BloodBankDashboard/CoordinationWithHospitals"
-import CollaborationWithBloodBanks from "./pages/BloodBankDashboard/CollaborationWithBloodBanks"
-import EditingDonorInformation from "./pages/BloodBankDashboard/EditingDonorInformation"
 
 function App() {
 
@@ -30,16 +22,6 @@ function App() {
         <Route path="/update-personal-info" element={<UpdatePersonalInfo />} />
         <Route path="/schedule-appointment" element={<ScheduleAppointment />} />
         <Route path="/donation-history" element={<DonationHistory />} />
-
-        {/* Bloodbank */}
-        <Route path="/blood-bank-dashboard" element={<BloodBankDashboard />} />
-        <Route path="/blood-stock-management" element={<BloodStockManagement />} />
-        <Route path="/tracking-blood-requests" element={<TrackingBloodRequests />} />
-        <Route path="/certifying-donors" element={<CertifyingDonors />} />
-        <Route path="/sending-notifications-to-donors" element={<SendingNotificationsToDonors />} />
-        <Route path="/coordination-with-hospitals" element={<CoordinationWithHospitals />} />
-        <Route path="/collaboration-with-blood-banks" element={<CollaborationWithBloodBanks />} />
-        <Route path="/editing-donor-information" element={<EditingDonorInformation />} />
       </Routes>
     </BrowserRouter>
   )
--- src/pages/History/History.tsx
+++ src/pages/History/History.tsx
@@ -1,7 +1,6 @@
-import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { Badge } from "@/components/ui/badge"
-import { Calendar, MapPin, Droplet } from "lucide-react"
+import {  MapPin, Droplet } from "lucide-react"
 
 export default function DonationHistory() {
     const donations = [
